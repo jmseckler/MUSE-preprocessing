54aa191066a3a1d45851ec22ccefd3e0f9189989
James <133902179+jmseckler@users.noreply.github.com>
2024-09-05 01:08:44 -0400

    Add files via upload
    Added the function that lets you process entire directories of runs, and also create flythroughs from movies (Linux Only)

--- data_surveyor.py
+++ data_surveyor.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from datetime import datetime
 import cv2 as cv
 import numpy as np
-import sys, os, getpass, ast, json
+import sys, os, getpass, ast, json, glob
 import matplotlib.pyplot as plt
 
 
@@ -14,6 +14,8 @@ import matplotlib.pyplot as plt
 cmdInputs = {
 	'-o':{"name":"Override Output","types":['str'],"names":['path'],"variable":["./output/"],"active":False,"tooltips":"Changes output directory"},	
 	'-su':{"name":"Data Survey","types":[],"names":[],"variable":[],"active":False,"tooltips":"Perform Data Survey"},
+	'-r':{"name":"Recursive","types":[],"names":[],"variable":[],"active":False,"tooltips":"Finds all folders in the "},
+	'-f':{"name":"Flythrough","types":[],"names":[],"variable":[],"active":False,"tooltips":"Creates a video flythrough (Linux Only)"},
 	}
 
 #Define the directory structure which the program will use. Version 2.1 will stop wiping all previous data to go with efficency
@@ -25,6 +27,29 @@ surveyFolder = metaFolder + 'survey/'
 
 dataConversionTags = ['means','focus','difference','histogram','shift']
 
+fly_font = cv.FONT_HERSHEY_SIMPLEX
+fly_color = (255,255,255)
+fly_thickness = 2
+fly_font_scale = 1
+fly_index_x = -50
+fly_index_y = 50
+fly_scale = 5
+
+
+scalebar_index_x = -50
+scalebar_index_y = -250
+scalebar_length_pixels = 200
+scalebar_length = int(scalebar_length_pixels * 0.9)
+scalebar_color = (255,255,255)
+scalebar_thickness = 5
+scalebar_text_width = 118
+scalebar_text_offset = (scalebar_length_pixels - scalebar_text_width) // 2
+
+
+header_thickness = 10
+header_font_scale = 3
+
+
 dataQualtyCheck = {
 	"Mean Intensity":"means",
 	"Laplacian Variance":"focus",
@@ -32,9 +57,25 @@ dataQualtyCheck = {
 	}
 
 def setup():
+	global fname
 	inputParser()
-	check_output_directory_structure_and_load_all_metadata()
-	validate_data_structure_and_metadata()
+	
+	if cmdInputs['-r']['active']:
+		flist = ms.findAllDir(base_path)
+		for f in flist:
+			print(f)
+			fname = f
+			variableEncode()
+			check_output_directory_structure_and_load_all_metadata()
+			validate_data_structure_and_metadata()			
+			if cmdInputs['-f']['active']:
+				create_flythrough_video_for_data_visualization()
+	else:
+		variableEncode()
+		check_output_directory_structure_and_load_all_metadata()
+		validate_data_structure_and_metadata()
+		if cmdInputs['-f']['active']:
+			create_flythrough_video_for_data_visualization()
 
 
 def inputParser():
@@ -73,7 +114,6 @@ def inputParser():
 					print(f"Input {tag} has failed to read in input values, using defaults...")
 		elif tag == '-jms':
 			base_path = '/media/' + getpass.getuser() + '/' + sys.argv[2] + '/data/'
-	variableEncode()
 
 def variableEncode():
 	global outpath
@@ -86,22 +126,31 @@ def variableEncode():
 	if not outpath.endswith((fname + '/')):
 		outpath = outpath + fname + '/'
 	
+	
 
 
 def printHelp():
 	print("This is a help for Seckler Post Processing Software.")
 	print("It expects to accept the input from MUSE REVA Preprocessing Software.")
-	print("Command: python post_processing.py <File Name> <Path to Data> <Run Array> <Options>")
+	print("Command: python post_processing.py <File Name> <Path to Data> <Options>")
 	print("")
 	for entry in cmdInputs:
 		print(generateHelpString(entry,cmdInputs[entry]))
 	quit()
 
+def generateHelpString(tag,entry):
+	helpString = ''
+	helpString += '-' + tag + ' '
+	for e in entry['names']:
+		helpString += '<' + e + '> '
+	helpString += '		'
+	helpString += entry['tooltips'] + ' '
+	return helpString
+
 
 def check_output_directory_structure_and_load_all_metadata():
 	print("Setting up output folder and loading all previously collected metadata...")
 	global data, metaPath, oldPath, imgPath, inPath, surveyPath, tmpPath
-	
 	inPath = base_path + fname + '/'
 	metaPath = outpath + metaFolder
 	oldPath = outpath + oldFolder
@@ -116,7 +165,6 @@ def check_output_directory_structure_and_load_all_metadata():
 	ms.make_directory(surveyPath)
 	ms.make_directory(tmpPath)
 	ms.replace_directory(imgPath)
-	
 	data = data_loader_from_json()	
 
 
@@ -171,7 +219,8 @@ def validate_data_structure_and_metadata():
 		passer = calculate_global_adjacent_image_difference_for_all_images()
 	if 'histogram' not in data:
 		passer = calculate_histogram_for_all_data()
-	
+	if 'height' not in data or 'width' not in data:
+		passer = determine_pixel_dimensions_for_all_images()
 	if passer or cmdInputs['-su']['active']:
 		rewrite_data_survey_file_and_write_survey_images()
 
@@ -196,11 +245,15 @@ def calculate_global_means_for_all_images_and_save_attributes():
 	data['means'] = {}
 	data['length'] = {}
 	data['attr'] = {}
+	data['width'] = {}
+	data['height'] = {}
 	for zarrNumber in tqdm(allArray):
 		zpath = inPath + 'MUSE_stitched_acq_'  + str(zarrNumber) + '.zarr'
 		img, attrs = ms.get_image_from_zarr(zpath)
 		data['attr'][zarrNumber] = attrs
 		data['means'][zarrNumber] = np.zeros(img.shape[0])
+		data['width'][zarrNumber] = img.shape[1]
+		data['height'][zarrNumber] = img.shape[2]
 		
 		for i in range(img.shape[0]):
 			data['means'][zarrNumber][i] = np.mean(img[i])
@@ -256,6 +309,20 @@ def calculate_global_adjacent_image_difference_for_all_images():
 	return True
 
 
+def determine_pixel_dimensions_for_all_images():
+	global data
+	data['width'] = {}
+	data['height'] = {}
+	for zarrNumber in allArray:
+		zpath = inPath + 'MUSE_stitched_acq_'  + str(zarrNumber) + '.zarr'
+		img, attrs = ms.get_image_from_zarr(zpath)
+		data['width'][zarrNumber] = img.shape[1]
+		data['height'][zarrNumber] = img.shape[2]
+	data = data_saver_to_json(data)
+	return True
+	
+
+
 def calculate_histogram_for_all_data():
 	print('Calculating Histograms for all images')
 	global data
@@ -275,6 +342,124 @@ def calculate_histogram_for_all_data():
 	return True
 
 
+def determine_number_of_flythroughs_to_make():
+	global data
+	
+	flythrough_number = 0
+	flyThroughRuns = {}
+	width = 0
+	height = 0
+	flyThroughRuns[flythrough_number] = []
+	
+	for zarrNumber in allArray:
+		if len(flyThroughRuns[flythrough_number]) == 0 or (width == data['width'][zarrNumber] and height == data['height'][zarrNumber]):
+			flyThroughRuns[flythrough_number].append(zarrNumber)
+		else:
+			flythrough_number += 1
+			flyThroughRuns[flythrough_number] = []
+			flyThroughRuns[flythrough_number].append(zarrNumber)
+		width = data['width'][zarrNumber]
+		height = data['height'][zarrNumber]
+	return flyThroughRuns
+
+def create_flythrough_video_for_data_visualization():
+	global data, tmpPath
+	tmpPath = surveyPath + '/tmp/'
+	flythroughs = determine_number_of_flythroughs_to_make()
+	
+	for fly in tqdm(flythroughs):
+		FlyArrays = flythroughs[fly]
+		ms.replace_directory(tmpPath)
+		counter = 0
+		for zarrNumber in FlyArrays:
+			zpath = inPath + 'MUSE_stitched_acq_'  + str(zarrNumber) + '.zarr'
+			img, attrs = ms.get_image_from_zarr(zpath)
+			for i in range(data['length'][zarrNumber]+1):
+				image = np.array(img[i])
+				image = image / 16
+				
+				resolution = (int(image.shape[1] / fly_scale), int(image.shape[0] / fly_scale))
+				image = cv.resize(image, resolution, interpolation= cv.INTER_LINEAR)
+				image = overlay_acu_and_index_number_on_image(image,zarrNumber,i)
+				image = overlay_scalebar_on_image(image)
+				image = overlay_header_on_image(image)
+				
+				c = format_image_number_to_10000(counter)
+				
+				cv.imwrite(tmpPath + f"image_{c}.png",image)
+				counter += 1
+		compile_pngs_to_movie(fly)
+	ms.remove_directory(tmpPath)
+
+def overlay_acu_and_index_number_on_image(image,zarrNumber,i):
+	position = create_text_position(image.shape)
+	text = f"Acq#{zarrNumber}, Index#{i}"
+	cv.putText(image, text, position, fly_font, fly_font_scale, fly_color, fly_thickness, cv.LINE_AA)
+	return image
+
+
+def overlay_scalebar_on_image(image):
+	position = create_text_position(image.shape,False)
+	
+	start_point = position
+	end_point = (position[0] + scalebar_length_pixels, position[1])
+	cv.line(image, start_point, end_point, scalebar_color, scalebar_thickness)
+	
+	
+	text = f"{scalebar_length} um"
+	text_size = cv.getTextSize(text, fly_font, fly_font_scale, fly_thickness)[0]
+	scalebar_text_offset = (scalebar_length_pixels - text_size[0]) // 2
+	
+	text_position = (start_point[0] + scalebar_text_offset, start_point[1] - 10)
+	cv.putText(image, text, text_position, fly_font, fly_font_scale, fly_color, fly_thickness, cv.LINE_AA)
+	return image
+
+
+def overlay_header_on_image(image):
+	text = fname.capitalize()
+	text_size = cv.getTextSize(text, fly_font, header_font_scale, header_thickness)[0]
+	
+	image_width = image.shape[1]
+	text_x = (image_width - text_size[0]) // 2
+	text_y = 30 + text_size[1]  # 30 pixels padding from the top
+	cv.putText(image, text, (text_x, text_y), fly_font, header_font_scale, fly_color, header_thickness, cv.LINE_AA)
+	return image
+
+
+def create_text_position(shape,acq=True):
+	if acq:
+		x = fly_index_x
+		y = fly_index_y
+	else:
+		x = scalebar_index_x
+		y = scalebar_index_y
+
+		
+	if x < 0:
+		x = shape[0] + x
+	if y < 0:
+		y = shape[1] + y
+	return (y,x)
+		
+
+def format_image_number_to_10000(c):
+	if c < 10:
+		text = '000' + str(c)
+	elif c < 100:
+		text = '00' + str(c)
+	elif c < 1000:
+		text = '0' + str(c)
+	else:
+		text = str(c)
+	return text
+
+
+def compile_pngs_to_movie(runNumber):
+	cmd = f"ffmpeg -framerate 10 -i {tmpPath}image_%04d.png -c:v libx264 -r 30 -pix_fmt yuv420p {surveyPath}flythrough_{runNumber}.mp4"
+	stream = os.popen(cmd)
+	output = stream.read()
+	print(output)
+
 
 def rewrite_data_survey_file_and_write_survey_images():
 	print('Writing Log File and saving a selection of images from all zarr acquisitions')
@@ -286,13 +471,15 @@ def rewrite_data_survey_file_and_write_survey_images():
 	
 	logFILE.write(f"Quality Check Generated on {date_time_str} by {getpass.getuser()}\n")
 	
-	logFILE.write('Acquistion,# of Images,')
+	logFILE.write('Acquistion,# of Images,Width,Height,')
 	for tag in dataQualtyCheck:
 		logFILE.write(tag + ',,')
 	logFILE.write('\n')
 	for zarrNumber in allArray:
 		logFILE.write(zarrNumber + ',')
 		logFILE.write(str(int(data['length'][zarrNumber])) + ',')
+		logFILE.write(str(int(data['width'][zarrNumber])) + ',')
+		logFILE.write(str(int(data['height'][zarrNumber])) + ',')
 		for tag in dataQualtyCheck:
 			logFILE.write(prepareDataForSurvey(dataQualtyCheck[tag],zarrNumber) + ',')
 		logFILE.write('\n')
